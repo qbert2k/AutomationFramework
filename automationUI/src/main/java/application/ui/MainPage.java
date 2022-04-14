d8c6beb24db86476f58f41cac003a580300e69f0
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-04-13 20:56:35 -0400

    primeros pasos

--- automationUI/src/main/java/application/ui/MainPage.java
+++ automationUI/src/main/java/application/ui/MainPage.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class MainPage {
 
-    public static final Target USERNAME = Selectors.byCSS("User Name", "user");
+    public static final Target USERNAME = Selectors.byID("User Name", "user");
     public static final Target PASSWORD = Selectors.byID("Password", "pass");
     public static final Target LOGIN_BUTTON = Selectors.byID("Login Button", "loginButton");
 }
