de18355e85cd6dea8f27cc5f1e5ac29caa0f826a
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-04-13 20:55:13 -0400

    primeros pasos

---
+++ automationUI/src/main/java/application/stepDefinition/LoginStepDefinitions.java
@@ -0,0 +1,26 @@
+package application.stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+
+    @Given("the user enter to main page")
+    public void theUserEnterToMainPage() {
+        
+    }
+
+    @When("the user enter the user name and password")
+    public void theUserEnterTheUserNameAndPassword() {
+        
+    }
+
+    @Then("add employee page is displayed")
+    public void addEmployeePageIsDisplayed() {
+    }
+
+    @When("the user enter (.*) and (.*)")
+    public void theUserEnterUserNameAndPassword() {
+    }
+}
